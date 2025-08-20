6ceda24248725cd16298de725ce8bab2f3746d6c
usmaan-shahh <you@example.com>
2025-08-20 23:39:58 +0530

    Update MongoDB connection string in createDemoScholars script

--- server/scripts/createDemoScholars.js
+++ server/scripts/createDemoScholars.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.conn_string);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
